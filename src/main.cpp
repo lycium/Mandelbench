c665e9b94418974ef4433743ee7c7c7c3d83c7af
Thomas Ludwig (8700k) <thomas.ludwig@gmail.com>
2022-11-23 00:27:48 +0100

    Tweaked colouring, rendering half animation range (i.e. no duplicate frames), saving out frame times, better comments etc

--- src/main.cpp
+++ src/main.cpp
@@ -20,35 +20,38 @@
 
 
 using u8 = unsigned char;
-using rgba8u = vec<4, u8>;
-using vec4f  = vec<4, float>;
+using rgb8u = vec<3, u8>;
+using vec4f = vec<4, float>;
 
-constexpr int xres = 720;
-constexpr int yres = 720;
-constexpr int num_frames  = 30 * 12;
+constexpr int res_multi = 8;
+constexpr int res_div   = 4;
+constexpr int xres = res_multi * 480 / res_div;
+constexpr int yres = res_multi * 270 / res_div;
+constexpr int num_frames  = 30 * 12 * 1;
 constexpr int noise_size  = 1 << 8;
-constexpr int num_samples = 6 * 6 * 6;
-constexpr double inv_num_samples = 1.0 / num_samples;
+constexpr int num_samples = 6*6*6; // 6*6*6*6;
+constexpr double inv_samples = 1.0 / num_samples;
+constexpr double aspect = (double)yres / (double)xres;
 
 const bool save_frames = true;
-const bool ramdrive = false;
+const bool ramdrive = true;
 const char * dir_prefix = (ramdrive ? "r:" : ".");
 
 
 
 inline double LinearMapping(double a, double b, double c, double d, double x) { return (x - a) / (b - a) * (d - c) + c; }
 
-inline vec4f ImageFunction(double frame, double x, double y, int num_frames, int xres, int yres)
+inline vec4f ImageFunction(double x, double y, double frame, int xres, int yres, int num_frames)
 {
 	constexpr int num_iters = 4096;
 
 	const double time  = LinearMapping(0, num_frames, 0.0, 3.141592653589793238 * 2, frame);
 	const double time2 = std::cos(3.141592653589793238 - time) * 0.5 + 0.5;
-	const double scale = std::exp(time2 * -12.0);
+	const double scale = std::exp(time2 * -11.0);
 	const vec2d centre = vec2d(-0.761574, -0.0847596);
 	const vec2d z0 = vec2d(
 		LinearMapping(0, xres, -scale, scale, x),
-		LinearMapping(0, yres, scale, -scale, y)) + centre;
+		LinearMapping(0, yres, scale, -scale, y) * aspect) + centre;
 
 	// Fast early out for main cardioid and period 2 bulb
 	// Ref: https://en.wikipedia.org/wiki/Plotting_algorithms_for_the_Mandelbrot_set#Cardioid_/_bulb_checking
@@ -60,17 +63,23 @@ inline vec4f ImageFunction(double frame, double x, double y, int num_frames, int
 		return vec4f(0);
 	else
 	{
+		constexpr double R = 25; // escape radius
+
 		vec2d z = z0;
 		int iteration = 0;
-		for (; iteration < num_iters && (dot(z, z) < 25 * 25); iteration++)
+		for (; iteration < num_iters && (dot(z, z) < R * R); iteration++)
 			z = vec2d(z.x() * z.x() - z.y() * z.y(), 2 * z.x() * z.y()) + z0;
 
 		// Binary decomposition colouring, see https://mathr.co.uk/mandelbrot/book-draft/#binary-decomposition
 		const int binary = iteration == num_iters ? 0 : (z.y() > 0);
 
-		const vec4f colours[2] = { vec4f(160, 100, 200, 256) / 256, vec4f(137, 25, 100, 256) / 256 };
-		const vec4f col_out = colours[iteration % 2] * binary;
-		return col_out * col_out * 3.0f;
+		const double dwell = iteration + 1 - std::log2(0.5 * std::log(dot(z, z)));
+		float dwell_loop = (float)std::cos(dwell * 3.141592653589793238 * 2 * 0.125f) * 0.5f + 0.5f;
+		dwell_loop = std::isnormal(dwell_loop) ? dwell_loop : 0;
+
+		const vec4f colours[2] = { vec4f(220, 80, 40, 256) / 256, vec4f(80, 50, 220, 256) / 256 };
+		const vec4f col_out = colours[iteration % 2];
+		return col_out * col_out * (3.0f * binary) - dwell_loop * 0.05f;
 	}
 }
 
@@ -120,57 +129,58 @@ void Hilbert(const vec2i dx, const vec2i dy, vec2i p, int size, std::vector<int>
 
 
 void RenderThreadFunc(
-	const int f,
+	const int frame,
 	const vec2d * const samples,
 	const uint16_t * const noise,
 	std::atomic<int> * const counter,
-	rgba8u * const image_out)
+	rgb8u * const image_out)
 {
 	// Get rounded up number of buckets in x and y
-	constexpr int bucket_size = 8;
-	const int x_buckets = (xres + bucket_size - 1) / bucket_size;
-	const int y_buckets = (yres + bucket_size - 1) / bucket_size;
-	const int num_buckets = x_buckets * y_buckets;
+	constexpr int bucket_size = 4;
+	constexpr int x_buckets = (xres + bucket_size - 1) / bucket_size;
+	constexpr int y_buckets = (yres + bucket_size - 1) / bucket_size;
+	constexpr int loop_frames = num_frames / 2 + 1;
+	constexpr int num_buckets = x_buckets * y_buckets;
 
 	while (true)
 	{
 		// Get the next bucket index atomically and exit if we're done
 		const int bucket_i = counter->fetch_add(1);
-		if (bucket_i >= num_buckets)
-			break;
+		if (bucket_i >= num_buckets * loop_frames)
+				break;
 
 		// Get pixel ranges for current bucket
-		const int bucket_y  = bucket_i / x_buckets;
-		const int bucket_x  = bucket_i - x_buckets * bucket_y;
-		const int bucket_x0 = bucket_x * bucket_size, bucket_x1 = std::min(bucket_x0 + bucket_size, xres);
-		const int bucket_y0 = bucket_y * bucket_size, bucket_y1 = std::min(bucket_y0 + bucket_size, yres);
+		const int
+			bucket_y  = bucket_i / x_buckets,
+			bucket_x  = bucket_i - x_buckets * bucket_y,
+			bucket_x0 = bucket_x * bucket_size, bucket_x1 = std::min(bucket_x0 + bucket_size, xres),
+			bucket_y0 = bucket_y * bucket_size, bucket_y1 = std::min(bucket_y0 + bucket_size, yres);
 
-		for (int y = bucket_y0; y < bucket_y1; ++y)
-		for (int x = bucket_x0; x < bucket_x1; ++x)
+		for (int pix_y = bucket_y0; pix_y < bucket_y1; ++pix_y)
+		for (int pix_x = bucket_x0; pix_x < bucket_x1; ++pix_x)
 		{
-			const double n = noise[(y % noise_size) * noise_size + (x % noise_size)] * (1.0 / 65536);
+			const double n = noise[(pix_y % noise_size) * noise_size + (pix_x % noise_size)] * (1.0 / 65536);
 
 			vec4f sum = 0;
 			for (int s = 0; s < num_samples; ++s)
 			{
-				double i = s * inv_num_samples + n; i = (i < 1) ? i : i - 1;
-				double j = samples[s].x()      + n; j = (j < 1) ? j : j - 1;
-				double k = samples[s].y()      + n; k = (k < 1) ? k : k - 1;
+				double i = s * inv_samples + n; i = (i < 1) ? i : i - 1;
+				double j = samples[s].x()  + n; j = (j < 1) ? j : j - 1;
+				double k = samples[s].y()  + n; k = (k < 1) ? k : k - 1;
 
 				sum += ImageFunction(
-					f + 0.5 + triDist(i),
-					x + 0.5 + triDist(j),
-					y + 0.5 + triDist(k),
-					num_frames, xres, yres);
+					pix_x + 0.5 + triDist(i),
+					pix_y + 0.5 + triDist(j),
+					frame + 0.5 + triDist(k),
+					xres, yres, num_frames);
 			}
-			sum *= inv_num_samples;
+			sum *= inv_samples;
 
-			image_out[y * xres + x] =
+			image_out[pix_y * xres + pix_x] =
 			{
-				std::min(255, (int)(std::sqrt(sum.x()) * 256)),
-				std::min(255, (int)(std::sqrt(sum.y()) * 256)),
-				std::min(255, (int)(std::sqrt(sum.z()) * 256)),
-				255 //std::min(255, (int)(sum.w() * 256))
+				std::max(0, std::min(255, (int)(std::sqrt(sum.x()) * 256))),
+				std::max(0, std::min(255, (int)(std::sqrt(sum.y()) * 256))),
+				std::max(0, std::min(255, (int)(std::sqrt(sum.z()) * 256))),
 			};
 		}
 	}
@@ -189,12 +199,14 @@ int main(int argc, char ** argv)
 #endif
 	printf("Rendering %d frames at res %d x %d with %d samples per pixel\n", num_frames, xres, yres, num_samples);
 
-	std::vector<rgba8u> image(xres * yres);
+	std::vector<rgb8u> image(xres * yres);
 
+	// Hammsersley sequence: https://en.wikipedia.org/wiki/Low-discrepancy_sequence#Hammersley_set
 	std::vector<vec2d> samples(num_samples);
 	for (int s = 0; s < num_samples; s++)
 		samples[s] = { RadicalInverse<2>(s), RadicalInverse<3>(s) };
 
+	// Approximate blue noise by recursive addition of golden ratio
 	std::vector<uint16_t> noise(noise_size * noise_size);
 	{
 		std::vector<int> hilbert_vals;
@@ -211,28 +223,39 @@ int main(int argc, char ** argv)
 
 	std::vector<std::thread> render_threads(num_threads);
 
-	const auto bench_start = std::chrono::system_clock::now();
+	FILE * frame_times_file = fopen("frame_times.csv", "w");
 
-	for (int f = 0; f < num_frames; ++f)
+	const auto bench_start = std::chrono::system_clock::now();
+	double total_render_time = 0;
+
+	//for (int frame = 0; frame < num_frames; ++frame)
+	for (int frame = 0; frame <= num_frames / 2; ++frame)
 	{
 		const auto t_start = std::chrono::system_clock::now();
 		{
 			std::atomic<int> counter = { 0 };
-			for (int z = 0; z < num_threads; ++z) render_threads[z] = std::thread(RenderThreadFunc, f, &samples[0], &noise[0], &counter, &image[0]);
+			for (int z = 0; z < num_threads; ++z) render_threads[z] = std::thread(RenderThreadFunc, frame, &samples[0], &noise[0], &counter, &image[0]);
 			for (int z = 0; z < num_threads; ++z) render_threads[z].join();
 		}
 		const std::chrono::duration<double> elapsed_time = std::chrono::system_clock::now() - t_start;
+		printf("Frame %d took %.2f seconds\n", frame, elapsed_time.count());
+		total_render_time += elapsed_time.count();
 
 		char filename[256];
-		sprintf(filename, "%s/frames/frame%04d.png", dir_prefix, f);
-		if (save_frames) stbi_write_png(filename, xres, yres, 4, &image[0], xres * 4);
+		sprintf(filename, "%s/frames/frame%04d.png", dir_prefix, frame);
+		if (save_frames) stbi_write_png(filename, xres, yres, 3, &image[0], xres * 3);
 
-		printf("Frame %d took %.2f seconds\n", f, elapsed_time.count());
+		fprintf(frame_times_file, "%d, %f\n", frame, elapsed_time.count());
 	}
 	printf("\n");
 
+	fclose(frame_times_file);
+
 	const std::chrono::duration<double> elapsed_time = std::chrono::system_clock::now() - bench_start;
-	printf("Rendering animation took %.2f seconds\n", elapsed_time.count());
+	printf("Rendering animation took %.2f seconds (%.2f including saving)\n", total_render_time, elapsed_time.count());
+
+	FILE * stats_f = fopen("benchmark_stats.txt", "w"); fprintf(stats_f, "%f\n%f\n", total_render_time, elapsed_time.count());
+	fclose(stats_f);
 
 	return 0;
 }
